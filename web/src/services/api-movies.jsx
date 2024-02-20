f641bcb06eca4f2f9158b2a194fb8be6f8161b39
Nadine Estrella Vacas <nadinestrella@gmail.com>
2024-02-20 11:23:33 +0100

    fetch

--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,36 +1,54 @@
 // login
 
-const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
-  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
-    .then(() => {
-      // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-      return {
-        success: true,
-        movies: [
-          {
-            id: '1',
-            title: 'Gambita de dama',
-            genre: 'Drama',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg'
-          },
-          {
-            id: '2',
-            title: 'Friends',
-            genre: 'Comedia',
-            image:
-              '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg'
-          }
-        ]
-      };
-    });
-};
+//const getMoviesFromApi = () => {
 
-const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
-};
+// console.log('Se están pidiendo las películas de la app');
+// CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
 
-export default objToExport;
+// return fetch('http://localhost:4000')
+//   .then((response) => response.json())
+//   .then(() => {
+//     // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
+//     return {
+//       success: true,
+//       movies: [
+//         {
+//           id: '1',
+//           title: 'Gambita de dama',
+//           genre: 'Drama',
+//           image:
+//             '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg',
+//         },
+//         {
+//           id: '2',
+//           title: 'Friends',
+//           genre: 'Comedia',
+//           image:
+//             '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg',
+//         },
+//       ],
+//     };
+//  });
+//};
+
+// const objToExport = {
+//   getMoviesFromApi: getMoviesFromApi,
+// };
+
+// export default objToExport;
+
+const loadJson = async () => {
+  try {
+    const url = 'http://localhost:4000';
+    const res = await fetch(url);
+    if (res.ok) {
+      const datos = await res.json();
+      console.log(datos);
+    } else {
+      new Error(res.status);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+loadJson();
