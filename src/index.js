5aefadf5d2ce39cda4087b15953ecd5d3612d271
Ángela Ávila Lasanta <lasantavispa@gmail.com>
2024-02-22 11:16:57 +0100

    4.5

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql2/promise');
 
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
 
+
+async function getConnection() {
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    database: 'netflix',
+    user: 'root',
+    password: 'Localhost',
+  });
+  await connection.connect();
+
+  console.log(
+    `Conexión establecida con la base de datos (identificador=${connection.threadId})`
+  );
+  return connection;
+}
+
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+server.get('/movies', async (req, res) => {
+  console.log('Pidiendo a la base de datos información de las movies.');
+  let sql = 'SELECT * FROM movies';
+  const connection = await getConnection();
+  const [results] = await connection.query(sql [req.query]);
+  res.json(results);
+  connection.end();
 });
--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,54 +1,18 @@
 // login
 
-//const getMoviesFromApi = () => {
+const getMoviesFromApi = () => {
+  console.log('Se están pidiendo las películas de la app');
+  // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
 
-// console.log('Se están pidiendo las películas de la app');
-// CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-
-// return fetch('http://localhost:4000')
-//   .then((response) => response.json())
-//   .then(() => {
-//     // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
-//     return {
-//       success: true,
-//       movies: [
-//         {
-//           id: '1',
-//           title: 'Gambita de dama',
-//           genre: 'Drama',
-//           image:
-//             '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/gambito-de-dama.jpg',
-//         },
-//         {
-//           id: '2',
-//           title: 'Friends',
-//           genre: 'Comedia',
-//           image:
-//             '//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/images/friends.jpg',
-//         },
-//       ],
-//     };
-//  });
-//};
-
-// const objToExport = {
-//   getMoviesFromApi: getMoviesFromApi,
-// };
-
-// export default objToExport;
-
-const loadJson = async () => {
-  try {
-    const url = 'http://localhost:4000';
-    const res = await fetch(url);
-    if (res.ok) {
-      const datos = await res.json();
-      console.log(datos);
-    } else {
-      new Error(res.status);
-    }
-  } catch (err) {
-    console.log(err);
-  }
+  return fetch('http://localhost:4000')
+    .then((response) => response.json())
+    .then((data) => {
+      return data;
+    });
 };
-loadJson();
+
+const objToExport = {
+  getMoviesFromApi: getMoviesFromApi,
+};
+
+export default objToExport;
